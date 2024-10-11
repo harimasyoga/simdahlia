335e1eb46fba07abab6a6a50f677d6744beb2cd8
hyoga999 <yogaharimas@gamil.com>
2024-10-11 20:07:34 +0700

    tambah menu penawaran harga 111024

--- application/controllers/Transaksi.php
+++ application/controllers/Transaksi.php
@@ -154,6 +154,16 @@ class Transaksi extends CI_Controller
 		
 	}
 
+	function Insert_penawaran()
+	{
+		if($this->session->userdata('username'))
+		{ 
+			$result = $this->m_transaksi->save_penawaran();
+			echo json_encode($result);
+		}
+		
+	}
+
 	function load_data()
 	{
 		$jenis        = $this->uri->segment(3);
@@ -293,9 +303,128 @@ class Transaksi extends CI_Controller
 				$data[] = $row;
 				$i++;
 			}
-		
-		
-		} else if ($jenis == "trs_so_detail") {
+		}else if ($jenis == "penawaran") 
+		{						
+				$query = $this->db->query("SELECT*FROM penawaran_header a 
+				join penawaran_detail b on a.no_penawaran=b.no_penawaran
+				ORDER BY a.tgl_penawaran,b.id_penawaran_det")->result();
+	
+				$i               = 1;
+				foreach ($query as $r) 
+				{
+	
+					$rinci_stok  = $this->db->query("SELECT*FROM penawaran_detail where no_penawaran ='$r->no_penawaran'");
+	
+					$hrg_sat = 0;
+					foreach ($rinci_stok->result() as $row_detail) 
+					{
+						$hrg_sat   += $row_detail->hrg_sat;
+					}		
+	
+					$total_hrg_sat = $hrg_sat;
+					$total_all     = $total_hrg_sat;
+	
+					$id              = "'$r->id_header_penawaran'";
+					$no_penawaran    = "'$r->no_penawaran'";
+	
+					if($r->acc_owner=='N')
+					{
+						$btn2   = 'btn-warning';
+						$i2     = '<i class="fas fa-lock"></i>';
+					} else {
+						$btn2   = 'btn-success';
+						$i2     = '<i class="fas fa-check-circle"></i>';
+					}
+					
+					if (in_array($this->session->userdata('username'), ['bumagda','developer']))
+					{
+						// $urll2 = "onclick=open_modal('$r->id_header_penawaran','$r->no_penawaran')";
+						$urll2 = "onclick=acc_inv('$r->no_penawaran','$r->acc_owner')";
+					} else {
+						$urll2 = '';
+					}
+						
+					$row    = array();
+					$row[]  = '<div class="text-center">'.$i.'</div>';
+					$row[]  = '<div class="text-center">'.$r->no_penawaran.'</div>';
+					$row[]  = '<div class="text-center">'.$r->tgl_penawaran.'</div>';
+					$row[]  = '<div class="text-center">'.$r->kpd.'</div>';
+	
+					// // Pembayaran
+					// $bayar = $this->db->query("SELECT SUM(jumlah_bayar) AS byr_beli from trs_bayar_inv_beli where no_inv_beli='$r->no_inv_beli' GROUP BY no_inv_beli");
+	
+					// if ($r->acc_owner == "N") 
+					// {
+					// 	$txtB            = 'btn-light';
+					// 	$txtT            = '-';
+					// 	$kurang_bayar    = '';
+					// }else{
+	
+					// 	if($bayar->num_rows() == 0){
+					// 		$txtB           = 'btn-danger';
+					// 		$txtT           = 'BELUM BAYAR';
+					// 		$kurang_bayar   = '';
+					// 	}
+						
+					// 	if($bayar->num_rows() > 0){
+					// 		if($bayar->row()->byr_beli == round($total_all)){
+					// 			$txtB            = 'btn-success';
+					// 			$txtT            = 'LUNAS';
+					// 			$kurang_bayar    = '';
+					// 		}else{
+					// 			$txtB            = 'btn-warning';
+					// 			$txtT            = 'DI CICIL';
+					// 			$kurang_bayar    = '<br><span style="color:#ff5733">'.number_format($total_all-$bayar->row()->byr_beli,0,',','.').'</span>';
+					// 		}
+					// 	}
+					// }
+					// $row[] = '<div class="text-center">
+					// 	<button type="button" class="btn btn-xs '.$txtB.'" style="font-weight:bold" >'.$txtT.'</button><br>
+					// </div>';
+	
+	
+					// $row[]  = '<div class="text-center">'.$pajak.'</div>';
+	
+					$row[]  = '
+							<div class="text-center"><a style="text-align: center;" class="btn btn-sm '.$btn2.' " '.$urll2.' title="VERIFIKASI DATA" ><b>'.$i2.' </b> </a><span style="font-size:1px;color:transparent">'.$r->acc_owner.'</span><div>';
+	
+					$btncetak ='<a target="_blank" class="btn btn-sm btn-danger" href="' . base_url("
+					Transaksi/Cetak_penawaran?no_penawaran="."$r->no_penawaran"."") . '" title="Cetak" ><i class="fas fa-print"></i> </a>';
+	
+					$btnEdit = '<a class="btn btn-sm btn-warning" onclick="edit_data(' . $id . ',' . $no_penawaran . ')" title="EDIT DATA" >
+					<b><i class="fa fa-edit"></i> </b></a>';
+	
+					$btnHapus = '<button type="button" title="DELETE"  onclick="deleteData(' . $id . ',' . $no_penawaran . ')" class="btn btn-secondary btn-sm">
+					<i class="fa fa-trash-alt"></i></button> ';
+						
+					if (in_array($this->session->userdata('level'), ['Admin','konsul_keu','User','Keuangan1']))
+					{
+						if ($r->acc_owner == "N") 
+						{						
+							$row[] = '<div class="text-center">'.$btnEdit.' '.$btncetak.' '.$btnHapus.'</div>';
+						}else{
+	
+							if($bayar->num_rows() == 0)
+							{
+								$row[] = '<div class="text-center">'.$btnEdit.' '.$btncetak.' '.$btnHapus.'</div>';
+							}else{
+								$row[] = '<div class="text-center">'.$btnEdit.' '.$btncetak.'</div>';
+	
+							}
+							
+							
+						}
+	
+					}else{
+						$row[] = '<div class="text-center"></div>';
+					}
+					
+					$data[] = $row;
+					$i++;
+				}
+			
+			
+			} else if ($jenis == "trs_so_detail") {
 			$query = $this->db->query("SELECT d.id AS id_po_detail,p.kode_mc,d.tgl_so,p.nm_produk,d.status_so,COUNT(s.rpt) AS c_rpt,l.nm_pelanggan,s.* FROM trs_po_detail d
 			INNER JOIN trs_so_detail s ON d.no_po=s.no_po AND d.kode_po=s.kode_po AND d.no_so=s.no_so AND d.id_produk=s.id_produk
 			INNER JOIN m_produk p ON d.id_produk=p.id_produk
@@ -675,6 +804,227 @@ class Transaksi extends CI_Controller
 		// $this->m_fungsi->_mpdf($html);
 		$this->m_fungsi->template_kop('ORDER PEMBELIAN',$no_po,$html,'P','1');
 		// $this->m_fungsi->mPDFP($html);
+	}
+	
+	function Cetak_penawaran()
+	{
+		$no_penawaran  = $_GET['no_penawaran'];
+
+        $query_header = $this->db->query("SELECT*FROM penawaran_header a 
+				join penawaran_detail b on a.no_penawaran=b.no_penawaran
+				where a.no_penawaran = '$no_penawaran'
+				ORDER BY a.tgl_penawaran,b.id_penawaran_det");
+        
+        $data = $query_header->row();
+        
+        $query_detail = $this->db->query("SELECT*FROM penawaran_detail where no_penawaran= '$no_penawaran'");
+
+		$html = '';
+
+		if ($query_header->num_rows() > 0) 
+		{
+
+			$html .= "<table style=\"border-collapse:collapse;font-family: Century Gothic; font-size:12px; color:#000;\" width=\"100%\"  border=\"0\" cellspacing=\"0\" cellpadding=\"0\" align=\"center\">
+			 <thead>
+				<tr><td>&nbsp; </td></tr>
+				<tr><td>&nbsp; </td></tr>
+				<tr>
+					<td rowspan=\"5\" >
+						<img src=\"" . base_url() . "assets/gambar/logo.png\"  width=\"80\" height=\"70\" />
+					</td>
+				</tr>
+				
+				<tr><td>&nbsp;</td></tr>
+			 </table>";
+			
+			 $html .= "<table style=\"border-collapse:collapse;font-family: Century Gothic; font-size:12px; color:#000;\" width=\"100%\"  border=\"\" cellspacing=\"0\" cellpadding=\"0\" >
+			 <thead>
+				  <tr><td ><b>CV. AUNGERAH ABADI</b></td></tr>
+				  <tr><td >&nbsp; </td></tr>
+			 </table>";
+			 
+			 $html .= "<table style=\"border-collapse:collapse;font-family: Century Gothic; font-size:12px; color:#000;\" width=\"100%\"  border=\"\" cellspacing=\"0\" cellpadding=\"0\" >
+			 <thead>
+				  <tr>
+					<td width=\"50%\">Surakarta, ".$this->m_fungsi->tanggal_format_indonesia($data->tgl_penawaran) ."</td>
+					<td width=\"50%\"></td>
+				  </tr>
+				  <tr><td >&nbsp; </td><td></td></tr>
+
+				  <tr><td >Kepada :</td> <td></td></tr>
+				  <tr><td >&nbsp; </td> <td></td></tr>
+				  <tr><td >".$data->kpd."</td> <td></td></tr>
+				  <tr><td >".$data->alamat."</td> <td></td></tr>
+
+				  <tr><td >&nbsp; </td><td></td></tr>
+
+				  <tr><td >Hal : ".$data->hal."</td> <td></td></tr>
+				  
+				  
+				  <tr><td >&nbsp; </td><td></td></tr>
+				  <tr><td >Dengan Hormat,</td> <td></td></tr>
+				  <tr><td >&nbsp; </td><td></td></tr>
+				  <tr><td >Berikut kami berikan penawaran harga botol yang diminta :</td> <td></td></tr>
+				  <tr><td >&nbsp; </td><td></td></tr>
+			 </table>";
+			
+			$html .= '<table width="100%" border="1" cellspacing="1" cellpadding="3" style="border-collapse:collapse;font-size:12px;font-family: ;">
+                        <tr style="background-color: #cccccc">
+							<th align="center">NO</th>
+							<th align="center">Nama barang</th>
+							<th align="center">Spesifikasi</th>
+                            <th align="center">HARGA SATUAN</th>
+                            <th align="center">MoQ</th>
+                            <th align="center">Note</th>
+						</tr>';
+						
+			$no              = 1;
+			$jum_all    = 0;			
+			foreach ($query_detail->result() as $r) 
+			{
+				$html .= '<tr>
+						<td align="center">' . $no . '</td>
+						<td align="">' . $r->nm_barang . '</td>
+						<td align="">' . $r->spek . '</td>
+						<td align="center">Rp ' . number_format($r->hrg_sat, 0, ",", ".") . '</td>
+						<td align="center">' . number_format($r->moq, 0, ",", ".") . '</td>
+						<td align="">' . $r->note . '</td>
+					</tr>';
+
+				$jum_all += $r->jum;
+				$no++;
+			}
+
+			// $total_nominal = $jum_all;
+				
+			// if($data->pajak=='PPN')
+			// {
+			// 	$ppn_total    = ROUND($total_nominal *0.11);
+			// 	$pph_total    = 0;
+			// }else if($data->pajak=='PPN_PPH')
+			// {
+			// 	$ppn_total   = ROUND($total_nominal *0.11);
+			// 	$pph_total   = ROUND($total_nominal *0.02);
+			// }else{
+			// 	$ppn_total   = 0;
+			// 	$pph_total   = 0;
+			// }
+			
+			// $total_all     = $total_nominal - $data->diskon + $ppn_total - $pph_total;
+
+			// $html .='<tr style="">
+			// 			<td align="left" style="border:none" colspan="3"><b>&nbsp;</b></td>
+
+			// 			<td align="right" colspan="2" ><b>Sub Total</b></td>
+			// 			<td align="right" ><b>' . number_format($nominal_all, 0, ",", ".") . '</b></td>						
+			// 		</tr>
+			// 		<tr style="">
+			// 			<td align="left" style="border:none" colspan="3"><b>Catatan Tambahan :</b></td>
+				
+			// 			<td align="right" colspan="2"><b>Diskon</b></td>
+			// 			<td align="right" ><b>' . number_format($data->diskon, 0, ",", ".") . '</b></td>						
+			// 		</tr>
+			// 		<tr style="">
+			// 			<td align="left" rowspan="2" style="border:none" colspan="3"><b>'.$data->cttn.'</b></td>		
+			// 			<td align="right" colspan="2"><b>PPN</b></td>
+			// 			<td align="right" ><b>' . number_format($ppn_total, 0, ",", ".") . '</b></td>						
+			// 		</tr>
+			// 		<tr style="">
+			// 			<td align="right" colspan="2"><b>PPH</b></td>
+			// 			<td align="right" ><b>' . number_format($pph_total, 0, ",", ".") . '</b></td>						
+			// 		</tr>
+			// 		<tr style="">
+			// 			<td align="left" style="border:none" colspan="3"><b>&nbsp;</b></td>
+			// 			<td align="right" colspan="2"><b>Total</b></td>
+			// 			<td align="right" ><b>' . number_format($total_all, 0, ",", ".") . '</b></td>						
+			// 		</tr>
+
+					
+			// 		<tr style="border:none"><td align="right" colspan="6">&nbsp;</td></tr>
+			// 		<tr style="border:none"><td align="right" colspan="6">&nbsp;</td></tr>
+			// 		';
+			$html .= '
+					<tr style="border:none"><td align="right" colspan="6">&nbsp;</td></tr>
+			</table>';
+
+			
+			$html .= '<table width="100%" border="0" cellspacing="0" style="font-size:12px;font-family: ;">
+			<tr> <td align="left">Keterangan :</td> </tr>
+			<tr style="border:none" > <td align="left"><textarea rows="4" cols="50">'.$data->ket.'</textarea></td> </tr>
+           
+            </table><br>';
+
+			$html .='<table width="100%" border="0" cellspacing="1" cellpadding="3" style="border-collapse:collapse;font-size:12px;font-family: ;">
+
+			<tr style="border:none"><td >Demikian penawaran harga dari kami,kabar selanjutnya dari bapak/ibu kami tunggu
+Terimakasih.</td></tr>
+			<tr style="border:none"><td >&nbsp;</td></tr>
+			</table>';
+			
+			
+			$html .= '<table width="100%" border="0" cellspacing="0" style="font-size:12px;font-family: ;">
+			<tr>
+                <td width="15%" align="left">&nbsp;</td>
+                <td width="30%" align="center">Hormat Kami</td>
+                <td width="10%" >&nbsp;</td>
+                <td width="30%" align="center"></td>
+                <td width="15%" >&nbsp;</td>
+            </tr>
+			<tr>
+                <td colspan="5">&nbsp;</td>
+            </tr>
+			<tr>
+                <td colspan="5">&nbsp;</td>
+            </tr>
+			<tr>
+                <td colspan="5">&nbsp;</td>
+            </tr>
+			<tr>
+                <td colspan="5">&nbsp;</td>
+            </tr>
+			<tr>
+                <td align="left">&nbsp;</td>
+                <td align="center" style="border-top: 1px solid black;">T. Agung R</td>
+                <td align="left">&nbsp;</td>
+                <td align="center" style=""></td>
+                <td >&nbsp;</td>
+            </tr>
+           
+            </table><br>';
+
+			
+		} else {
+			$html .= '<h1> Data Kosong </h1>';
+		}
+
+		// echo $html;
+		// $this->m_fungsi->_mpdf($html);
+		// $this->m_fungsi->template_kop('PENAWARAN HARGA',$no_penawaran,$html,'P','1');
+		// $this->m_fungsi->mPDFP($html);
+
+		$judul          = 'PENAWARAN HARGA';
+		$position       = 'P';
+		$cekpdf         = 1;
+
+		switch ($cekpdf) {
+			case 0;
+				echo ("<title>$judul</title>");
+				echo ($html);
+				break;
+
+			case 1;
+				$this->m_fungsi->_mpdf_hari($position, 'A4', $judul, $html, 'PENAWARAN HARGA.pdf', 5, 5, 5, 10);
+			
+
+
+				break;
+			case 2;
+				header("Cache-Control: no-cache, no-store, must-revalidate");
+				header("Content-Type: application/vnd-ms-excel");
+				header("Content-Disposition: attachment; filename= $judul.xls");
+				$this->load->view('app/master_cetak', $data);
+				break;
+		}
 	}
 
 
